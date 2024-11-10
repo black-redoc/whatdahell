35c644e8a7c16e659c7f9e12311cb31a19079c78
black <joseb.twelve@gmail.com>
2024-11-09 23:36:05 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,95 @@
+import os
+import openai
+from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import Response
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastAPI()
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai_client = openai.OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
+
+
+async def get_request_body(request: Request, Body: str = Form(...)):
+    form = await request.form()
+    incoming_msg = Body.strip().lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    return form, resp, incoming_msg, msg
+
+
+def get_twilio_response(media_url: str):
+    audio_response = requests.get(media_url, auth=HTTPBasicAuth(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN))
+    return audio_response.content
+
+
+def write_adio_content_to_file(audio_content: bytes) -> str:
+    path_to_file = "received_audio.mp3"
+    with open(path_to_file, "wb") as f:
+        f.write(audio_content)
+    return path_to_file
+
+
+def get_transcription(path_to_file: str) -> str:
+    audio_file = open(path_to_file, "rb")
+    transcript = openai_client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+    transcription = transcript.text
+    if len(transcription.split(" ")) > 40:
+        transcription = transcription.strip()
+
+        # Process transcription with GPT-3.5
+        prompt = f"Summarize the following text in less of 40 words:\n\n{transcription}"
+        response = openai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+        )
+        summary = response.choices[0].message.content.strip()
+        transcription = summary.split("Summary:")[-1].strip()
+        os.remove("received_audio.mp3")
+    return transcription
+
+
+@app.get("/")
+async def index(request: Request):
+    return {"hello": "world"}
+
+
+@app.post("/whatsapp")
+async def whatsapp_webhook(request: Request, Body: str = Form(...), NumMedia: str = Form(default="0")):
+    (form, resp, _, msg) = await get_request_body(request, Body)
+
+    if int(NumMedia) > 0:
+        media_url = form.get("MediaUrl0")
+        media_type = form.get("MediaContentType0")
+
+        if "audio" in media_type:
+            # Download the audio file
+            audio_content = get_twilio_response(media_url)
+
+            # Save the audio file
+            path_to_file = write_adio_content_to_file(audio_content)
+            try:
+                # Transcribe the audio file with Whisper
+                response_text = get_transcription(path_to_file)
+            except Exception as e:
+                print(e)
+                response_text = f"Error processing audio: {e}"
+        else:
+            response_text = f"Invalid media type: {media_type}."
+    else:
+        response_text = "I'm sorry, I didn't understand that. Send an audio, please. This bot will transcribe it."
+
+    msg.body(response_text)
+    return Response(content=str(resp), media_type="application/xml")
---
+++ test_app.py
@@ -0,0 +1,103 @@
+from dotenv import load_dotenv
+import pytest
+from fastapi.testclient import TestClient
+from unittest.mock import AsyncMock, patch
+from twilio.twiml.messaging_response import MessagingResponse, Message
+import xmltodict
+
+load_dotenv()
+from .app import app, get_request_body, get_transcription, get_twilio_response, write_adio_content_to_file
+
+client = TestClient(app)
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"hello": "world"}
+
+
+def test_whatsapp_webhook_with_200():
+    # Prepare form data to send with the POST request
+    form_data = {
+        "Body": "Hello from WhatsApp!",
+        "NumMedia": "2",
+        "MediaUrl0": "https://example.com/audio.mp3",
+        "MediaContentType0": "audio/mp3",
+    }
+
+    # Send a POST request to the /whatsapp endpoint
+    response = client.post("/whatsapp", data=form_data)
+
+    # Assert the response status code and content
+    assert response.status_code == 200
+    result = xmltodict.parse(response.text)
+    expeted_result = """
+    Error processing audio: Error code: 400 - {'error': {'message': "Invalid file format. Supported formats: ['flac', 'm4a', 'mp3', 'mp4', 'mpeg', 'mpga', 'oga', 'ogg', 'wav', 'webm']", 'type': 'invalid_request_error', 'param': None, 'code': None}}
+    """.strip()
+    assert result["Response"]["Message"]["Body"] == expeted_result
+
+
+def test_whatsapp_webhook_with_422():
+    response = client.post("/whatsapp", json={"Body": "Hello, world!"})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [{"loc": ["body", "Body"], "msg": "field required", "type": "value_error.missing"}]
+    }
+
+
+@pytest.mark.asyncio
+async def test_get_request_body():
+    response_mock = AsyncMock()
+    body_mock = "Hello from WhatsApp!"
+    expected_msg = "hello from whatsapp!"
+    form, resp, incoming_msg, msg = await get_request_body(response_mock, Body=body_mock)
+    assert form is not None
+    assert isinstance(resp, MessagingResponse)
+    assert incoming_msg == expected_msg
+    assert isinstance(msg, Message)
+
+
+def test_get_twilio_response():
+    with patch("requests.get") as mock_get:
+        mock_get.return_value.content = b"audio content"
+        expected_response = b"audio content"
+        response = get_twilio_response("https://example.com/audio.mp3")
+        assert response == expected_response
+
+
+def test_get_transcription():
+    with (
+        patch("openai.OpenAI") as mock_openai,
+        patch("os.remove"),
+        patch("builtins.open"),
+        patch("openai.resources.audio.Audio.transcriptions"),
+        patch("openai.resources.audio.transcriptions.Transcriptions.create") as mock_transcriptions_create,
+    ):
+        mock_transcriptions_create.return_value.text = "transcription"
+        mock_openai.audio.transcriptions.create.return_value.text = "transcription"
+        expected_transcription = "transcription"
+        transcription = get_transcription("path_to_file")
+        assert transcription == expected_transcription
+
+
+def test_write_adio_content_to_file():
+    audio_content = "audio content"
+    expected_path_to_file = "received_audio.mp3"
+    with patch("builtins.open"):
+        path_to_file = write_adio_content_to_file(audio_content)
+        assert path_to_file == expected_path_to_file
+
+
+def test_get_transcription():
+    with (
+        patch("openai.OpenAI") as mock_openai,
+        patch("builtins.open"),
+        patch("openai.resources.audio.Audio.transcriptions"),
+        patch("openai.resources.audio.transcriptions.Transcriptions.create") as mock_transcriptions_create,
+    ):
+        mock_transcriptions_create.return_value.text = "transcription"
+        mock_openai.audio.transcriptions.create.return_value.text = "transcription"
+        expected_transcription = "transcription"
+        transcription = get_transcription("path_to_file")
+        assert transcription == expected_transcription
